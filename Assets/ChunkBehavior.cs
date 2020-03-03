54e2fc2864b8dd10fddccf4b8979c1b27e92f21a
gabe <cofflag@wwu.edu>
2020-03-03 13:52:10 -0800

    First Commit, Leaving School

---
+++ Assets/ChunkBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkBehavior : MonoBehaviour
+{
+    public BoundsInt chunkBounds;
+    public string blockPath;
+    private GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Resources.Load<GameObject>(blockPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateChunk()
+    {
+        for (int i = chunkBounds.min.x; i < chunkBounds.max.x; i++)
+        {
+            for (int j = chunkBounds.min.y; j < chunkBounds.max.y; j++)
+            {
+                for (int k = chunkBounds.min.z; k < chunkBounds.max.z; k++)
+                {
+                    Instantiate<GameObject>(block, new Vector3(i, j, k), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
