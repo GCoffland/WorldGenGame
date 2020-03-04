b1a58271b2ae3b4a827e0a7a7d0a860d93df40a8
gabe <cofflag@wwu.edu>
2020-03-04 12:53:52 -0800

    Trying to get multiple colors to show on combined mesh. Work in progress.

---
+++ Assets/Code/ChunkBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkBehavior : MonoBehaviour
+{
+    public BoundsInt chunkBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateChunk();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateChunk()
+    {
+        Globals.setAllBlockTypeActiveState(true);
+        MeshFilter[] meshFilters = Globals.getAllTypesOfMeshFilters();
+        List<CombineInstance>[] combineInstances = new List<CombineInstance>[Globals.BLOCK_TYPE.NUM_OF_BLOCK_TYPES];
+
+        for (int i = chunkBounds.min.x; i < chunkBounds.max.x; i++)
+        {
+            for (int j = chunkBounds.min.y; j < chunkBounds.max.y; j++)
+            {
+                for (int k = chunkBounds.min.z; k < chunkBounds.max.z; k++)
+                {
+                    int num = Random.Range(0, Globals.BLOCK_TYPE.NUM_OF_BLOCK_TYPES);
+                    meshFilters[num].gameObject.transform.position = new Vector3(i, j, k);
+                    //combineInstances[num].Add(new CombineInstance());
+                    //combineInstances[num][combineInstances[num].Count-1].mesh = meshFilters[num].sharedMesh;
+                    //combine[(i * chunkBounds.size.y * chunkBounds.size.z) + (j * chunkBounds.size.y) + k].mesh = meshFilters[num].sharedMesh;
+                    //combine[(i * chunkBounds.size.y * chunkBounds.size.z) + (j * chunkBounds.size.y) + k].transform = meshFilters[num].transform.localToWorldMatrix;
+                }
+            }
+        }
+
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        //transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.SetActive(true);
+        Globals.setAllBlockTypeActiveState(false);
+    }
+}
---
+++ Assets/Code/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals
+{
+    public static class BLOCK_TYPE
+    {
+        public static readonly int NUM_OF_BLOCK_TYPES = 2;
+        public static GameObject DIRT = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Blocks/Dirt"));
+        public static GameObject GRASS = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Blocks/Grass"));
+    }
+
+    public static MeshFilter[] getAllTypesOfMeshFilters()
+    {
+        MeshFilter[] ret = new MeshFilter[BLOCK_TYPE.NUM_OF_BLOCK_TYPES];
+        ret[0] = BLOCK_TYPE.DIRT.GetComponent<MeshFilter>();
+        ret[1] = BLOCK_TYPE.GRASS.GetComponent<MeshFilter>();
+        return ret;
+    }
+
+    public static void setAllBlockTypeActiveState(bool val)
+    {
+        BLOCK_TYPE.DIRT.SetActive(val);
+        BLOCK_TYPE.GRASS.SetActive(val);
+    }
+}
