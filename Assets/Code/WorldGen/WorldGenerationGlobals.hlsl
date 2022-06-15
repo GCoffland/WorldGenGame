2b834d8a0383dde350bb4ce4e2fbaf0c42c46a21
gcoffland <gcoffland@gmail.com>
2022-06-15 13:10:41 -0700

    Added hlsl globals file, forgot in last commits

---
+++ Assets/Code/WorldGen/WorldGenerationGlobals.hlsl
@@ -0,0 +1,23 @@
+//
+// This file was automatically generated. Please don't edit by hand. Execute Editor command [ Edit > Rendering > Generate Shader Includes ] instead
+//
+
+#ifndef WORLDGENERATIONGLOBALS_CS_HLSL
+#define WORLDGENERATIONGLOBALS_CS_HLSL
+//
+// WorldGeneration.WorldGenerationGlobals:  static fields
+//
+#define MAX_RENDER_DISTANCE (100)
+#define MAX_ACTIVE_CHUNK_COUNT (1000)
+
+// Generated from WorldGeneration.VertexBufferStruct
+// PackingRules = Exact
+struct VertexBufferStruct
+{
+    float3 position;
+    float3 normal;
+    float2 texCoord;
+};
+
+
+#endif
