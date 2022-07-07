bb781721b0728351918bdbd965db300323890b2f
gcoffland <gcoffland@gmail.com>
2022-07-06 21:42:03 -0700

    Cleaned up a bit more, need to modify for some functionality, but pretty good

--- Assets/Code/WorldGen/ChunkModel.cs
+++ Assets/Code/WorldGen/ChunkModel.cs
@@ -14,39 +14,17 @@ using System.Linq;
 public class ChunkModel : MonoBehaviour
 {
     public event Action<int, int, int> OnBlockChanged;
+    public event Action OnModelGenerated;
 
     [SerializeField]
     private Chunk chunk;
 
     public NativeArray<uint> blockMap { get; private set; }
-    public NativeArray<uint>[] borderBlockMaps { get; private set; } = new NativeArray<uint>[6];
-    private bool[] borderBlockMapIsClean = new bool[6];
 
-    private void Awake()
-    {
-        OnBlockChanged += notifyBlockChangeHelper;
-    }
 
     private void OnDestroy()
     {
         if (blockMap.IsCreated) blockMap.Dispose();
-        for (int n = 0; n < 6; n++)
-        {
-            if (borderBlockMaps[n].IsCreated) borderBlockMaps[n].Dispose();
-        }
-    }
-
-    private void notifyBlockChangeHelper(int x, int y, int z)
-    {
-        if (IsBorderBlock(new Vector3Int(x, y, z), out DIRECTION dir) && chunk.neighbors[(int)dir] != null)
-        {
-            notifyNeighborOfChange(dir);
-        }
-    }
-
-    private void notifyNeighborOfChange(DIRECTION dir)
-    {
-        chunk.neighbors[(int)dir].model.borderBlockMapIsClean[(int)dir + 1 - 2 * ((int)dir % 2)] = false;
     }
 
     public async Task GenerateModel()
@@ -54,13 +32,7 @@ public class ChunkModel : MonoBehaviour
         NativeArray<uint> tempBlockMap = new NativeArray<uint>(WorldGenerationGlobals.BlockMapLength, Allocator.Persistent);
         await ModelGenerator.Singleton.GenerateBlockmap(tempBlockMap, chunk.bounds);
         blockMap = tempBlockMap;
-        for(int i = 0; i < StaticDefinitions.Directions.Length; i++)  // Nofiy neighbors
-        {
-            if(chunk.neighbors[i] != null)
-            {
-                notifyNeighborOfChange((DIRECTION)i);
-            }
-        }
+        OnModelGenerated?.Invoke();
     }
 
     public static bool IsBorderBlock(Vector3Int pos, out DIRECTION dir_index)
@@ -82,37 +54,18 @@ public class ChunkModel : MonoBehaviour
         return false;
     }
 
-    public void FetchBorderBlockmap(DIRECTION dir)
+    public void FetchBorderBlockmap(DIRECTION dir, ref NativeArray<uint> borderBlockMap)
     {
         Vector3Int current = Vector3Int.zero;
-        if (!borderBlockMaps[(int)dir].IsCreated)
+        int side_index = (int)dir / 2;
+        current[side_index] = ((((int)dir + 1) % 2) * 63);
+        int axis_one = (side_index + 1) % 3;
+        int axis_two = (side_index + 2) % 3;
+        for (current[axis_one] = 0; current[axis_one] < WorldGenerationGlobals.ChunkSize[axis_one]; current[axis_one]++)
         {
-            borderBlockMaps[(int)dir] = new NativeArray<uint>(WorldGenerationGlobals.ChunkSideAreas[(int)dir], Allocator.Persistent);
-        }
-        if (chunk.neighbors[(int)dir] != null && chunk.neighbors[(int)dir].model.blockMap.IsCreated)
-        {
-            int side_index = (int)dir / 2;
-            current[side_index] = ((((int)dir + 1) % 2) * 63);
-            int axis_one = (side_index + 1) % 3;
-            int axis_two = (side_index + 2) % 3;
-            for (current[axis_one] = 0; current[axis_one] < WorldGenerationGlobals.ChunkSize[axis_one]; current[axis_one]++)
+            for (current[axis_two] = 0; current[axis_two] < WorldGenerationGlobals.ChunkSize[axis_two]; current[axis_two]++)
             {
-                for (current[axis_two] = 0; current[axis_two] < WorldGenerationGlobals.ChunkSize[axis_two]; current[axis_two]++)
-                {
-                    borderBlockMaps[(int)dir].SetAsChunk(current[axis_one], current[axis_two], 0, chunk.neighbors[(int)dir].model.GetBlock(current[0], current[1], current[2]));
-                }
-            }
-        }
-    }
-
-    public void TryFetchBorderBlockmaps()
-    {
-        for (int n = 0; n < 6; n++)
-        {
-            if (!borderBlockMapIsClean[n])
-            {
-                FetchBorderBlockmap((DIRECTION)n);
-                borderBlockMapIsClean[n] = true;
+                borderBlockMap.SetAsChunk(current[axis_one], current[axis_two], 0, chunk.neighbors[(int)dir].model.GetBlock(current[0], current[1], current[2]));
             }
         }
     }
--- Assets/Code/WorldGen/ChunkRenderer.cs
+++ Assets/Code/WorldGen/ChunkRenderer.cs
@@ -24,12 +24,15 @@ public class ChunkRenderer : MonoBehaviour
     [SerializeField]
     private Mesh defaultMesh;
 
+    private NativeArray<uint>[] borderBlockMaps = new NativeArray<uint>[6];
+    private bool[] borderBlockMapIsClean = new bool[6];
     private Task meshGenTask;
 
     void Awake()
     {
         SetDefaultMesh();
         chunk.model.OnBlockChanged += genMeshHelper;
+        chunk.model.OnModelGenerated += onModelGenerated;
     }
 
     private bool modelChangedThisFrame = false;
@@ -43,9 +46,54 @@ public class ChunkRenderer : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        for (int n = 0; n < 6; n++)
+        {
+            if (borderBlockMaps[n].IsCreated) borderBlockMaps[n].Dispose();
+        }
+    }
+
+    private void onModelGenerated()
+    {
+        for (int i = 0; i < StaticDefinitions.Directions.Length; i++)  // Nofiy neighbors
+        {
+            if (chunk.neighbors[i] != null)
+            {
+                notifyNeighborOfChange((DIRECTION)i);
+            }
+        }
+    }
+
     private void genMeshHelper(int x, int y, int z)
     {
         modelChangedThisFrame = true;
+        if (ChunkModel.IsBorderBlock(new Vector3Int(x, y, z), out DIRECTION dir) && chunk.neighbors[(int)dir] != null)
+        {
+            notifyNeighborOfChange(dir);
+        }
+    }
+
+    private void notifyNeighborOfChange(DIRECTION dir)
+    {
+        chunk.neighbors[(int)dir].renderer.borderBlockMapIsClean[(int)dir + 1 - 2 * ((int)dir % 2)] = false;
+        _ = chunk.neighbors[(int)dir].renderer.GenerateMesh();
+    }
+
+    public void TryFetchBorderBlockmaps()
+    {
+        for (int n = 0; n < StaticDefinitions.Directions.Length; n++)
+        {
+            if (!borderBlockMaps[n].IsCreated)
+            {
+                borderBlockMaps[n] = new NativeArray<uint>(WorldGenerationGlobals.ChunkSideAreas[n], Allocator.Persistent);
+            }
+            if (!borderBlockMapIsClean[n] && chunk.neighbors[n] != null && chunk.neighbors[n].model.blockMap.IsCreated)
+            {
+                chunk.model.FetchBorderBlockmap((DIRECTION)n, ref borderBlockMaps[n]);
+                borderBlockMapIsClean[n] = true;
+            }
+        }
     }
 
     public void SetDefaultMesh()
@@ -64,8 +112,8 @@ public class ChunkRenderer : MonoBehaviour
     public async Task GenerateMesh()
     {
         Debug.Log("Generating Mesh for Chunk: " + chunk.index);
-        chunk.model.TryFetchBorderBlockmaps();
-        Mesh mesh = await MeshGenerator.Singleton.GenerateMeshData(chunk.model.blockMap, chunk.model.borderBlockMaps);
+        TryFetchBorderBlockmaps();
+        Mesh mesh = await MeshGenerator.Singleton.GenerateMeshData(chunk.model.blockMap, borderBlockMaps);
         meshFilter.mesh.Clear();
         meshFilter.mesh = mesh;
         meshCollider.sharedMesh = mesh;
